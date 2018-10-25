979c35bafea4ce8a338f6d5f56115f2aacd1254f
Tama-github <ludovic.burg@univ-tlse3.fr>
2018-10-25 19:26:17 +0200

    removing wrong folder from repository

--- ExternalLibrarys/OpenMesh-7.1/build/src/OpenMesh/Apps/Smoothing/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tama/Moteure/animation/TEngine/ExternalLibrarys/OpenMesh-7.1/src/OpenMesh/Apps/Smoothing
-# Build directory: /home/tama/Moteure/animation/TEngine/ExternalLibrarys/OpenMesh-7.1/build/src/OpenMesh/Apps/Smoothing
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
